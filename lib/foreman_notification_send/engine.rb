cf708e1794679ced308d59c62d0f3453edb55973
Alexander Olofsson <alexander.olofsson@liu.se>
2024-10-07 15:27:11 +0200

    Prepare for zeitwerk

--- lib/foreman_notification_send/engine.rb
+++ lib/foreman_notification_send/engine.rb
@@ -14,35 +14,37 @@ module ForemanNotificationSend
     end
 
     # rubocop:disable Metrics/BlockLength
-    initializer 'foreman_notification_send.register_plugin', before: :finisher_hook do |_app|
-      Foreman::Plugin.register :foreman_notification_send do
-        requires_foreman '>= 3.0'
+    initializer 'foreman_notification_send.register_plugin', before: :finisher_hook do |app|
+      app.reloader.to_prepare do
+        Foreman::Plugin.register :foreman_notification_send do
+          requires_foreman '>= 3.12'
 
-        settings do
-          category :notification_send, N_('Notification Send') do
-            setting 'notification_send_enable',
-                    type: :boolean,
-                    description: N_('Enable'),
-                    default: false,
-                    full_name: N_('Enable')
+          settings do
+            category :notification_send, N_('Notification Send') do
+              setting 'notification_send_enable',
+                      type: :boolean,
+                      description: N_('Enable'),
+                      default: false,
+                      full_name: N_('Enable')
 
-            setting 'notification_send_target_url',
-                    type: :string,
-                    description: N_('Target URI'),
-                    default: 'https://matrix.org',
-                    full_name: N_('Target URI')
+              setting 'notification_send_target_url',
+                      type: :string,
+                      description: N_('Target URI'),
+                      default: 'https://matrix.org',
+                      full_name: N_('Target URI')
 
-            setting 'notification_send_target_room',
-                    type: :string,
-                    description: N_('Target Room'),
-                    default: '#test:matrix.org',
-                    full_name: N_('Target Room')
+              setting 'notification_send_target_room',
+                      type: :string,
+                      description: N_('Target Room'),
+                      default: '#test:matrix.org',
+                      full_name: N_('Target Room')
 
-            setting 'notification_send_token',
-                    type: :string,
-                    description: N_('Token'),
-                    default: 'syt_abcdefg',
-                    full_name: N_('Token')
+              setting 'notification_send_token',
+                      type: :string,
+                      description: N_('Token'),
+                      default: 'syt_abcdefg',
+                      full_name: N_('Token')
+            end
           end
         end
       end
